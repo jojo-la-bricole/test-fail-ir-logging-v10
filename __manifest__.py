0fed7bf316cdf03eea41602f2026218d25a5b5fd
jojo-la-bricole <42742979+jojo-la-bricole@users.noreply.github.com>
2019-03-07 13:27:22 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Emplddoyees',
+    'version': '1.1.3',
+    'category': 'Human Resources',
+    'sequence': 75,
+    'summary': 'Centralize employee information',
+    'description': "",
+    'website': 'https://www.odoo.com/page/employees',
+    'images': [
+        'images/hr_department.jpeg',
+        'images/hr_employee.jpeg',
+        'images/hr_job_position.jpeg',
+        'static/src/img/default_image.png',
+    ],
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'demo': [
+        
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
