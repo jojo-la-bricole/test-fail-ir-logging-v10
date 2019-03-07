1e77a9ff6a101cc449888a262cbf48abf41de7c4
jojo-la-bricole <42742979+jojo-la-bricole@users.noreply.github.com>
2019-03-07 13:26:26 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class Users(models.Model):
+    _name = 'res.users'
+    _inherit = ['res.users']
+
+    name2 = fields.Char(string='Name')
+    name3 = fields.Char(string='Name')
+    name4 = fields.Char(string='Name')
+    name5 = fields.Char(string='Name')
